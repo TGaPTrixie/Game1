74d8ae741b016a2bb738254c61ff9c4a63fc6e60
TGaPTrixie <ernoi.chryso@gmail.com>
2014-08-11 15:34:36 -0300

    Enemies asplode

---
+++ Assets/Scripts/MoveLeftAndRight.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveLeftAndRight : MonoBehaviour 
+{
+	public float speed = 20.0f;
+
+	void Start () 
+	{
+	
+	}
+
+	void Update () 
+	{
+		transform.position += Vector3.right*Input.GetAxis("Horizontal")*speed*Time.deltaTime;
+	}
+}
