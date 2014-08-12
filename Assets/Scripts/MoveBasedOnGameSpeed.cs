791e5273536fbd3df7fd328fc31842acf5ff3a53
TGaPTrixie <ernoi.chryso@gmail.com>
2014-08-11 21:56:07 -0300

    Score system and unified Speed system

---
+++ Assets/Scripts/MoveBasedOnGameSpeed.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveBasedOnGameSpeed : MonoBehaviour 
+{
+	public Vector3 direction = Vector3.forward;
+
+	void Start () 
+	{
+
+	}
+
+	void Update () 
+	{
+		transform.position += transform.rotation*(direction.normalized*GameManager.Instance.gameSpeed*Time.deltaTime);
+	}
+}
