4598d84aeb5bd2a0fd072f503fb276fe4e83f859
TGaPTrixie <ernoi.chryso@gmail.com>
2014-08-12 12:23:06 -0300

    Turned GameManager into a singleton, which opened pandora's box, what other horrors await us?

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,51 +3,98 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour 
 {
-	public static GameManager Instance;
+	public static GameManager Instance
+	{
+		get
+		{
+			if(_instance != null)
+			{
+				return _instance;
+			}
+			else
+			{
+				GameObject gameManager = new GameObject("GameManager");
+				_instance = gameManager.AddComponent<GameManager>();
+				return _instance;
+			}
+		}
+	}
+
+	private static GameManager _instance;
 
 	public float pointsPerUnitTravel = 1.0f;
 	public float gameSpeed = 10.0f;
+	public string titleScreenName = "TittleScreen";
+
+	[HideInInspector]
+	public int previousScore = 0;
 
 	private float score = 0.0f;
 	private static float highScore = 0.0f;
 	private bool gameOver = false;
 	private bool hasSaved = false;
-
-
+	
 	// Use this for initialization
 	void Start () 
 	{
-		Instance = this;
+		if (_instance != this)
+		{
+			if (_instance == null)
+			{
+				_instance = this;
+			}	
+			else
+			{
+				Destroy (gameObject);
+			}
+		}
 		LoadHighScore ();
+		DontDestroyOnLoad (gameObject);
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		if (GameObject.FindGameObjectWithTag ("Player") == null) 
+		if(Application.loadedLevelName != titleScreenName)
 		{
-			gameOver = true;
+			if (GameObject.FindGameObjectWithTag ("Player") == null) 
+			{
+				gameOver = true;
+			}
+			if (gameOver) 
+			{
+				if(!hasSaved)
+				{
+					SaveHighScore ();
+					previousScore = (int)score;
+					hasSaved = true;
+				}
+				if(Input.anyKeyDown)
+				{
+					Application.LoadLevel(titleScreenName);
+				}
+			}
+			if (!gameOver) 
+			{
+				score += pointsPerUnitTravel * gameSpeed * Time.deltaTime;
+				if (score > highScore)
+				{
+					highScore = score;
+				}
+			}
 		}
-		if (gameOver) 
+		else
 		{
-			if(!hasSaved)
-			{
-				SaveHighScore ();
-				hasSaved = true;
-			}
-			if(Input.anyKeyDown)
-			{
-				Application.LoadLevel(Application.loadedLevel);
-			}
+			//Reset Stuff for next game
+			ResetGame ();
 		}
-		if (!gameOver) 
-		{
-			score += pointsPerUnitTravel * gameSpeed * Time.deltaTime;
-			if (score > highScore)
-			{
-				highScore = score;
-			}
-		}
+	}
+
+	void ResetGame()
+	{
+		score = 0.0f;
+		gameOver = false;
+		hasSaved = false;
 	}
 
 	void SaveHighScore()
@@ -63,13 +110,16 @@ public class GameManager : MonoBehaviour
 
 	void OnGUI()
 	{
-		int currentScore = (int)score;
-		int currentHighScore = (int)highScore;
-		GUILayout.Label("Score: " + currentScore.ToString());
-		GUILayout.Label("Highscore: " + currentHighScore.ToString());
-		if (gameOver == true) 
+		if(Application.loadedLevelName != titleScreenName)
 		{
-			GUILayout.Label("Game Over! Press any key to reset!");
+			int currentScore = (int)score;
+			int currentHighScore = (int)highScore;
+			GUILayout.Label("Score: " + currentScore.ToString());
+			GUILayout.Label("Highscore: " + currentHighScore.ToString());
+			if (gameOver == true) 
+			{
+				GUILayout.Label("Game Over! Press any key to Quit!");
+			}
 		}
 	}
 }
