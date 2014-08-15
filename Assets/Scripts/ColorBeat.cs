0af3bbd5e5720b1fdc6a8e3412abf64ada8055f2
TGaPTrixie <ernoi.chryso@gmail.com>
2014-08-15 15:39:07 -0300

    Music Visualizer

---
+++ Assets/Scripts/ColorBeat.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorBeat : MonoBehaviour
+{
+	public int samples = 64;
+	public int channel = 0;
+	public int frecuencyChannel = 32;
+	public float amplitudeMultiplier = 1.0f;
+	public FFTWindow window;
+
+	private float originalFogDensity;
+
+	// Use this for initialization
+	void Start ()
+	{
+		originalFogDensity = RenderSettings.fogDensity;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		float [] data = new float[samples];
+		AudioListener.GetSpectrumData (data,channel,window);
+		RenderSettings.fogDensity = originalFogDensity + data [frecuencyChannel]*amplitudeMultiplier;
+	}
+}
--- Assets/Scripts/MoveBasedOnGameSpeed.cs
+++ Assets/Scripts/MoveBasedOnGameSpeed.cs
@@ -5,11 +5,6 @@ public class MoveBasedOnGameSpeed : MonoBehaviour
 {
 	public Vector3 direction = Vector3.forward;
 
-	void Start () 
-	{
-
-	}
-
 	void Update () 
 	{
 		transform.position += transform.rotation*(direction.normalized*GameManager.Instance.gameSpeed*Time.deltaTime);
