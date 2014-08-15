fd4e2a9adaa6a66a41911741c44b80c1e117c3f8
TGaPTrixie <ernoi.chryso@gmail.com>
2014-08-15 16:36:37 -0300

    Prep For highScores System

---
+++ Assets/Scripts/LoadLevelOnClick.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadLevelOnClick : MonoBehaviour
+{
+	public string levelName;
+
+	void Touched()
+	{
+		Application.LoadLevel(levelName);
+	}
+}
